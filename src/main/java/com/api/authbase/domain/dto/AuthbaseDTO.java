498a4e0fc938e2b3dbd9d82f049c891c308998f9
Sérgio Gomes <srochg@gmail.com>
2023-06-23 23:11:31 -0300

    Adding files

--- src/main/java/com/api/authbase/domain/dto/AuthbaseDTO.java
+++ src/main/java/com/api/authbase/domain/dto/AuthbaseDTO.java
@@ -1,6 +1,5 @@
 package com.api.authbase.domain.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
